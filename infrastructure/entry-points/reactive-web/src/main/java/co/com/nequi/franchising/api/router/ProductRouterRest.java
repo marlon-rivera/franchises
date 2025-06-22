8045c373d90f8c0085c5e017749a68d510bc1121
marlon.rivera1 <marlon.rivera@pragma.com.co>
2025-06-22 00:06:23 -0500

    feat(product): add OpenAPI documentation for create product endpoint

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/nequi/franchising/api/router/ProductRouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/nequi/franchising/api/router/ProductRouterRest.java
@@ -2,17 +2,48 @@ package co.com.nequi.franchising.api.router;
 
 import co.com.nequi.franchising.api.handler.ProductHandler;
 import co.com.nequi.franchising.utils.constants.ProductConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class ProductRouterRest {
 
+    @RouterOperations(
+            @RouterOperation(
+                    path = ProductConstants.ENDPOINT_CREATE_PRODUCT,
+                    method = POST,
+                    beanClass = ProductHandler.class,
+                    beanMethod = "saveProduct",
+                    operation = @Operation(
+                            summary = "Create Product",
+                            operationId = "createProduct",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Product created successfully"
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "400",
+                                            description = "Invalid product data"
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "500",
+                                            description = "Internal server error"
+                                    )
+                            }
+                    )
+            )
+    )
     @Bean
     public RouterFunction<ServerResponse> productRoutes(ProductHandler handler) {
         return route(POST(ProductConstants.ENDPOINT_CREATE_PRODUCT), handler::saveProduct);
